e27c478448abb7e39bc22f362ee876fc72bb11fd
mandalorian7117 <63601622+mandalorian7117@users.noreply.github.com>
2020-04-13 21:22:40 +0500

    Add files via upload

---
+++ precache-manifest.c6192b156e5d31f58afe0f7b80b0635d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32823d1281ce15c4b3f93d29429e303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5c005ed43c134d58de4",
+    "url": "/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "e5c005ed43c134d58de4",
+    "url": "/static/js/2.9465bc52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9465bc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6ee598424101f9e282",
+    "url": "/static/js/main.646ea285.chunk.js"
+  },
+  {
+    "revision": "b6656e9ee5f05057b532",
+    "url": "/static/js/runtime-main.0132ba8f.js"
+  }
+]);
