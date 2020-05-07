58f826ea2565448bd21ff10031961601aab251f3
Егор <63601622+mandalorian7117@users.noreply.github.com>
2020-05-07 16:27:21 +0500

    Add files via upload

---
+++ precache-manifest.3461ce7fa72a8edc4ff192622281a292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20288cb3b4113072e6c4296a1fabea82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c67481a5d42e864853",
+    "url": "/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "96c67481a5d42e864853",
+    "url": "/static/js/2.74635291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74635291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76f7daabb884778e9f1",
+    "url": "/static/js/main.0cac01d2.chunk.js"
+  },
+  {
+    "revision": "159f975ad8886a017ff8",
+    "url": "/static/js/runtime-main.69682bda.js"
+  }
+]);
