e706d94249f34c759cd72094de1bb4053897aa5c
Егор <63601622+mandalorian7117@users.noreply.github.com>
2020-05-14 16:07:25 +0500

    Add files via upload

---
+++ precache-manifest.9a9f48e77902c3f34083c04350c94329.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135f694c8747e4c0cc0283f5c68b0e18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e0d647ed79ba7984b80",
+    "url": "/static/css/2.47f050ad.chunk.css"
+  },
+  {
+    "revision": "22b0e0fd9e97f5b81fe0",
+    "url": "/static/css/main.048fa38a.chunk.css"
+  },
+  {
+    "revision": "8e0d647ed79ba7984b80",
+    "url": "/static/js/2.e8ddc61d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8ddc61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b0e0fd9e97f5b81fe0",
+    "url": "/static/js/main.ef050eb7.chunk.js"
+  },
+  {
+    "revision": "159f975ad8886a017ff8",
+    "url": "/static/js/runtime-main.69682bda.js"
+  },
+  {
+    "revision": "3a20d12e6947d53e4426a59c5a513144",
+    "url": "/static/media/angel.3a20d12e.png"
+  },
+  {
+    "revision": "007c104eb1ab9831d63466a257201291",
+    "url": "/static/media/bolnoy.007c104e.png"
+  },
+  {
+    "revision": "615257825a28c4f82f16f58a29b52f43",
+    "url": "/static/media/dead.61525782.png"
+  },
+  {
+    "revision": "3d75581f56db6dca558244a52036c85a",
+    "url": "/static/media/yandex.3d75581f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec04687809447a09d8b27db3813a7054.js"
+  "/precache-manifest.9a9f48e77902c3f34083c04350c94329.js"
 );
 
 self.addEventListener('message', (event) => {
