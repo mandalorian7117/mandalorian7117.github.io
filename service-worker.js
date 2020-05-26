38d71e44df8e9dd9b2b35f403e0b596d66046c95
Егор <63601622+mandalorian7117@users.noreply.github.com>
2020-05-26 18:47:50 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93ed3b3cb1b5a01eeb4d24aac56378b6.js"
+  "/precache-manifest.7bdf4e54f889ca36b6dd7d086cfc932e.js"
 );
 
 self.addEventListener('message', (event) => {
